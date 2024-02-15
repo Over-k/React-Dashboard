6b386830a8339d84bdb52250b8dd8db74d9084ca
khali ait miloud <you@example.com>
2024-02-15 16:25:16 +0100

    initialized react-app _Hosting

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Button from "./Components/Utils/Button"
+export default function app() {
+  return<div className="App">
+    <Button/>
+  </div>;
+}
---
+++ src/Routes.js
@@ -0,0 +1,6 @@
+import {Home, Dashboard} from "./Pages"
+const routes = [
+  { name: "Home", path:"home", element: <Home /> },
+  { name: "Dashboard",path:"dashboard", element: <Dashboard /> },
+];
+export default routes
